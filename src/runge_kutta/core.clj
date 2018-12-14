de218b737f37464e004670fdd2758ad6e1270882
Dogxx000 <scot0530@morris.umn.edu>
2018-12-14 00:37:15 -0600

    Add proto-repl-charts code and polynomialDE example

--- src/runge_kutta/core.clj
+++ src/runge_kutta/core.clj
@@ -1,4 +1,24 @@
-(ns runge-kutta.core)
+(ns runge-kutta.core
+    (:require [runge-kutta.numerical-methods :as nm]))
+
+(use 'prc)
+
+(defn poly-ex
+    "y'(t) = -2y^2 + ty + t^2"
+    [t y]
+    (+ (* -2 (* y y))
+       (* t y)
+       (* t t)))
+
+(proto-repl-charts.charts/line-chart
+    "RK4 Chart"
+    {"RK4" (nm/rk4 poly-ex {:t0 0 :y0 1} 1.0 0.1)}
+    {:labels (range 0 1 0.1)})
+
+(proto-repl-charts.table/table
+    "RK Table"
+    (nm/rk4 poly-ex {:t0 0 :y0 1} 1.0 0.1)
+    )
 
 (defn -main []
-  (println "Hello World"))
+  (println (nm/rk4 poly-ex {:t0 0 :y0 1} 1.0 0.1)))
