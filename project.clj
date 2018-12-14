9115c58c07dbc67b10386e9e5cd853a0f12e7cfb
Dogxx000 <scot0530@morris.umn.edu>
2018-12-14 00:35:45 -0600

    Remove auto-diff dependency, add proto-repl-charts

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject runge-kutta "0.1.0-SNAPSHOT"
     :description "ODE numerical methods including Runge-Kutta Order 4"
     :dependencies [[org.clojure/clojure "1.9.0"]
-                   [org.clojure/math.numeric-tower "0.0.4"]
-                   [clj-auto-diff "0.1.3"]]
+                   [proto-repl-charts "0.3.1"]]
     :main runge-kutta.core)
