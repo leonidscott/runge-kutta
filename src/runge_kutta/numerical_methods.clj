44a48a04c8ad1ae813bea2d943b4d4c4d31a2c48
Dogxx000 <scot0530@morris.umn.edu>
2018-12-14 00:36:40 -0600

    Add RK4

--- src/runge_kutta/numerical_methods.clj
+++ src/runge_kutta/numerical_methods.clj
@@ -1,9 +1,49 @@
-(ns runge-kutta.numerical-methods
-    (:require [clj-auto-diff :as d]
-              [clojure.math.numeric-tower :as math]))
+(ns runge-kutta.numerical-methods)
 
-(defn sqr
-    [x]
-    (math/expt x 2))
+(def ^:private output-points (atom []))
 
-(sqr 6)
+(defn- k1
+  "k1 = h f(ti yi)"
+  [f ti yi h]
+  (* (f ti yi) h))
+
+(defn- k2-3
+  "k2 = h f(ti + h/2, yi + k1/2)
+   k3 = h f(ti + h/2, yi + k2/2)"
+  [f ti yi h k]
+  (* (f (+ ti (/ h 2))
+        (+ yi (/ k 2)))
+     h))
+
+(defn- k4
+  "k4 = h f(ti, wi + k3)"
+  [f ti yi h k3]
+  (* (f ti (+ yi k3))
+     h))
+
+(defn- y-ip1
+  [f ti yi h]
+  (let [k1 (k1 f ti yi h)
+        k2 (k2-3 f ti yi h k1)
+        k3 (k2-3 f ti yi h k2)
+        k4 (k4 f ti yi h k3)]
+    (+ yi (/ (+ k1 (* 2 k2) (* 2 k3) k4)
+             6))))
+
+(defn rk4
+  "f = f(t,y) = dy/dt
+   ivp = {:x0 :y0}
+   input-points = [t0, t1, ..., tn]
+   h = stepsize"
+   [f {:keys [t0 y0]} domain h]
+   (reset! output-points [{:t t0 :y y0}])
+   (loop [ti t0
+          yi y0
+          current-step h]
+    (if (> (+ current-step h) domain)
+        @output-points
+        (let [yip1 (y-ip1 f ti yi h)]
+          (swap! output-points conj {:t (+ ti h) :y yip1})
+          (recur (+ ti h)
+                 yip1
+                 (+ h current-step))))))
